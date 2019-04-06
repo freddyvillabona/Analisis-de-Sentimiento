f6d1203441535f53224b82e02b80761fb15ac00b
Freddy <36832437+freddyvillabona@users.noreply.github.com>
2019-04-06 14:01:47 -0400

    Update extraccionTuits.R

--- extraccionTuits.R
+++ extraccionTuits.R
@@ -25,19 +25,12 @@ access_secret <- " "
 
 # Autenticación API TWITTER
 
-setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+options(httr_oauth_cache=TRUE)
 
-credentials_file <- "my_oauth.Rdata"
-if (file.exists(credentials_file)){
-  load(credentials_file)
-} else {
-  cred <- OAuthFactory$new(consumerKey = consumer_key, consumerSecret =
-                             consumer_secret, requestURL = reqURL, accessURL = accessURL, authURL = authURL)
-  cred$handshake(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
-  save(cred, file = credentials_file)
-}
+setup_twitter_oauth(consumer_key = consumerKey, consumer_secret = consumerSecret,
+                    access_token = accessToken, access_secret = accessSecret)
 
-# Recolecta tweets con palabra, frases y nombre de usuario
+# Consulta a la API
 tweets <- searchTwitter("venezuela", n=1000, lang="es",since="2019-01-19", until="2019-01-20")
 
 tweets
